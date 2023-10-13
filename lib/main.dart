c2159191a84c5162ae805973de846df4471fbd66
yourmyzoey <lovelyhj1124@duksung.ac.kr>
2023-10-13 14:43:54 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+
+import 'package:audio_streamer/audio_streamer.dart';
+import 'package:noise_meter/noise_meter.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(NoiseMeterApp());
+
+
+
+class NoiseMeterApp extends StatefulWidget {
+  @override
+  _NoiseMeterAppState createState() => _NoiseMeterAppState();
+}
+
+class _NoiseMeterAppState extends State<NoiseMeterApp> {
+  bool _isRecording = false;
+  NoiseReading? _latestReading;
+  StreamSubscription<NoiseReading>? _noiseSubscription;
+  NoiseMeter? noiseMeter;
+
+  @override
+  void dispose() {
+    _noiseSubscription?.cancel();
+    super.dispose();
+  }
+
+  void onData(NoiseReading noiseReading) =>
+      setState(() => _latestReading = noiseReading);
+
+  void onError(Object error) {
+    print(error);
+    stop();
+  }
+
+  /// Check if microphone permission is granted.
+  Future<bool> checkPermission() async => await Permission.microphone.isGranted;
+
+  /// Request the microphone permission.
+  Future<void> requestPermission() async =>
+      await Permission.microphone.request();
+
+  /// Start noise sampling.
+  Future<void> start() async {
+    // Create a noise meter, if not already done.
+    noiseMeter ??= NoiseMeter();
+
+    // Check permission to use the microphone.
+    //
+    // Remember to update the AndroidManifest file (Android) and the
+    // Info.plist and pod files (iOS).
+    if (!(await checkPermission())) await requestPermission();
+
+    // Listen to the noise stream.
+    _noiseSubscription = noiseMeter?.noise.listen(onData, onError: onError);
+    setState(() => _isRecording = true);
+  }
+
+  /// Stop sampling.
+  void stop() {
+    _noiseSubscription?.cancel();
+    setState(() => _isRecording = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    home: Scaffold(
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    margin: EdgeInsets.all(25),
+                    child: Column(children: [
+                      Container(
+                        child: Text(_isRecording ? "Mic: ON" : "Mic: OFF",
+                            style: TextStyle(fontSize: 25, color: Colors.blue)),
+                        margin: EdgeInsets.only(top: 20),
+                      ),
+                      Container(
+                        child: Text(
+                          'Noise: ${_latestReading?.meanDecibel.toStringAsFixed(2)} dB',
+                        ),
+                        margin: EdgeInsets.only(top: 20),
+                      ),
+                      Container(
+                        child: Text(
+                          'Max: ${_latestReading?.maxDecibel.toStringAsFixed(2)} dB',
+                        ),
+                      )
+                    ])),
+              ])),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _isRecording ? Colors.red : Colors.green,
+        child: _isRecording ? Icon(Icons.stop) : Icon(Icons.mic),
+        onPressed: _isRecording ? stop : start,
+      ),
+    ),
+  );
+}
---
+++ lib/noise_meter.dart
@@ -0,0 +1,56 @@
+library noise_meter;
+
+import 'dart:core';
+import 'dart:math';
+
+import 'package:audio_streamer/audio_streamer.dart';
+
+/// Holds a decibel value for a noise level reading.
+class NoiseReading {
+  late double _meanDecibel, _maxDecibel;
+
+  NoiseReading(List<double> volumes) {
+    // sort volumes such that the last element is max amplitude
+    volumes.sort();
+
+    // compute average peak-amplitude using the min and max amplitude
+    double min = volumes.first;
+    double max = volumes.last;
+    double mean = 0.5 * (min.abs() + max.abs());
+
+    // max amplitude is 2^15
+    double maxAmp = pow(2, 15) + 0.0;
+
+    _maxDecibel = 20 * log(maxAmp * max) * log10e;
+    _meanDecibel = 20 * log(maxAmp * mean) * log10e;
+  }
+
+  /// Maximum measured decibel reading.
+  double get maxDecibel => _maxDecibel;
+
+  /// Mean decibel across readings.
+  double get meanDecibel => _meanDecibel;
+
+  @override
+  String toString() =>
+      '$runtimeType - mean (dB): $meanDecibel, max (dB): $maxDecibel';
+}
+
+/// A [NoiseMeter] provides continuous access to noise reading via the [noise]
+/// stream.
+class NoiseMeter {
+  Stream<NoiseReading>? _stream;
+
+  /// Create a [NoiseMeter].
+  NoiseMeter();
+
+  /// The actual sampling rate.
+  Future<int> get sampleRate => AudioStreamer().actualSampleRate;
+
+  /// The stream of noise readings.
+  ///
+  /// Remember to obtain permission to use the microphone **BEFORE**
+  /// using this stream.
+  Stream<NoiseReading> get noise => _stream ??=
+      AudioStreamer().audioStream.map((buffer) => NoiseReading(buffer));
+}
